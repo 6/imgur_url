504224d9d7d94f13aa87c173b1c4ca08f491cbc4
Example User <peterghm@gmail.com>
2013-06-13 23:22:13 -0700

    parse image urls

--- lib/imgur_url/image.rb
+++ lib/imgur_url/image.rb
@@ -4,5 +4,21 @@ module ImgurUrl
     def initialize(url)
       @original_url = url
     end
+
+    def image_id
+      @image_id ||= original_url.match(%r{imgur\.com/(?:(?:gallery|download)/)?([a-zA-Z0-9]+)[^/]*}).andand[1]
+    end
+
+    def permalink_url
+      @permalink_url ||= "http://imgur.com/#{image_id}"
+    end
+
+    def direct_url
+      @direct_url ||= "http://i.imgur.com/#{image_id}.jpg"
+    end
+
+    def download_url
+      @download_url ||= "http://imgur.com/download/#{image_id}"
+    end
   end
 end
--- spec/lib/image_spec.rb
+++ spec/lib/image_spec.rb
@@ -2,8 +2,39 @@ require 'spec_helper'
 
 module ImgurUrl
   describe Image do
-    it "..." do
-      described_class.new("fff").original_url.should == "fff"
+    shared_examples "with a valid imgur image URL" do
+      subject { described_class.new(url) }
+
+      its(:original_url) { should == url }
+      its(:image_id) { should == "dhuY8" }
+      its(:permalink_url) { should == "http://imgur.com/dhuY8" }
+      its(:download_url) { should == "http://imgur.com/download/dhuY8" }
+      its(:direct_url) { should == "http://i.imgur.com/dhuY8.jpg" }
+    end
+
+    context "a default permalink URL" do
+      let(:url) { "http://imgur.com/dhuY8" }
+      it_behaves_like "with a valid imgur image URL"
+    end
+
+    context "a default permalink URL with query params" do
+      let(:url) { "http://imgur.com/dhuY8?tags" }
+      it_behaves_like "with a valid imgur image URL"
+    end
+
+    context "a direct URL" do
+      let(:url) { "http://i.imgur.com/dhuY8.jpg" }
+      it_behaves_like "with a valid imgur image URL"
+    end
+
+    context "a gallery URL" do
+      let(:url) { "http://imgur.com/gallery/dhuY8" }
+      it_behaves_like "with a valid imgur image URL"
+    end
+
+    context "a download URL" do
+      let(:url) { "http://imgur.com/download/dhuY8" }
+      it_behaves_like "with a valid imgur image URL"
     end
   end
 end
